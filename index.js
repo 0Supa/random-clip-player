97b8019ee3a18519d6e8cfa1684c7976fc813eac
Supa <36031171+0Supa@users.noreply.github.com>
2021-07-27 12:36:14 +0300

    v2

--- index.js
+++ index.js
@@ -25,6 +25,29 @@ app.get('/rcp/:channel', async (req, res) => {
     res.render('index', { clips })
 })
 
+app.use('/rcp/v2', express.static(__dirname + '/static'))
+
+app.get('/rcp/api/clips/:channel', async (req, res) => {
+    const channel = req.params.channel
+    if (!channel) return res.status(400).json({ error: 'you need to specify the channel name' })
+
+    const user = (await utils.helix(`users?login=${channel}`)).body.data
+    if (!user.length) return res.status(400).json({ error: 'invalid channel name' })
+
+    let clips
+    const cacheData = await utils.redis.get(`rc:clips:${user[0].id}`)
+    if (cacheData) {
+        clips = JSON.parse(cacheData)
+    } else {
+        clips = await getClips(user[0].id, 3)
+        if (clips.length < 5) return res.status(404).json({ error: 'the channel needs to have at least 5 clips' })
+
+        await utils.redis.set(`rc:clips:${user[0].id}`, JSON.stringify(clips), "EX", 86400)
+    }
+
+    res.json(clips)
+})
+
 async function getClips(channelID, reqs) {
     let i = 0
     let clips = []
---
+++ static/script.js
@@ -0,0 +1,54 @@
+const loading = document.getElementById("loading")
+const params = new URLSearchParams(window.location.search);
+const channel = params.get('channel')
+
+if (params.get('author') === '1') document.getElementById("author").style.display = "block";
+
+let clips = [];
+let shuffledClips = [];
+getClips()
+async function getClips() {
+    if (!channel) return error('you need to specify the channel name')
+    loading.style.display = "flex";
+    const res = await fetch(`${window.location.origin}/rcp/api/clips/${channel}`)
+    const data = await res.json()
+    loading.style.display = "none";
+    if (data.error) return error(data.error)
+    clips = data
+    random()
+}
+
+const clip = document.getElementById('clip');
+const by = document.getElementById('by');
+
+clip.addEventListener('ended', random, false);
+
+function error(text) {
+    const error = document.getElementById("error")
+    error.textContent = `Error: ${text}`
+}
+
+function play(source) {
+    clip.setAttribute('src', source);
+    clip.volume = 1
+}
+
+function random() {
+    by.innerHTML = ''
+    if (!shuffledClips.length) shuffledClips = shuffle(clips)
+    const clipData = shuffledClips.pop()
+
+    by.innerHTML = `Clipped by <span style="color: #9146ff">${clipData.by.length > 15 ? `${clipData.by.substring(0, 15)}..` : clipData.by}</span>`
+    play(`https://clips-media-assets2.twitch.tv/${clipData.id}.mp4`);
+}
+
+function shuffle(array) {
+    let res = [];
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        res[i] = array[j];
+        res[j] = temp;
+    }
+    return res;
+}
