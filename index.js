3e8d4282c2781dba3a2723af50c1ccaa10a31415
Supa <36031171+0Supa@users.noreply.github.com>
2021-07-27 14:41:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,27 +4,6 @@ const utils = require('./utils.js')
 
 app.set('view engine', 'ejs');
 
-app.get('/rcp/:channel', async (req, res) => {
-    const channel = req.params.channel
-    if (!channel) return res.status(400).send('You need to specify the channel name')
-
-    const user = (await utils.helix(`users?login=${channel}`)).body.data
-    if (!user.length) return res.status(400).send('Invalid channel name')
-
-    let clips
-    const cacheData = await utils.redis.get(`rc:clips:${user[0].id}`)
-    if (cacheData) {
-        clips = JSON.parse(cacheData)
-    } else {
-        clips = await getClips(user[0].id, 3)
-        if (clips.length < 5) return res.status(404).send('The channel needs to have at least 5 clips')
-
-        await utils.redis.set(`rc:clips:${user[0].id}`, JSON.stringify(clips), "EX", 86400)
-    }
-
-    res.render('index', { clips })
-})
-
 app.use('/rcp/v2', express.static(__dirname + '/static'))
 
 app.get('/rcp/api/clips/:channel', async (req, res) => {
@@ -46,6 +25,27 @@ app.get('/rcp/api/clips/:channel', async (req, res) => {
     }
 
     res.json(clips)
+})
+
+app.get('/rcp/:channel', async (req, res) => {
+    const channel = req.params.channel
+    if (!channel) return res.status(400).send('You need to specify the channel name')
+
+    const user = (await utils.helix(`users?login=${channel}`)).body.data
+    if (!user.length) return res.status(400).send('Invalid channel name')
+
+    let clips
+    const cacheData = await utils.redis.get(`rc:clips:${user[0].id}`)
+    if (cacheData) {
+        clips = JSON.parse(cacheData)
+    } else {
+        clips = await getClips(user[0].id, 3)
+        if (clips.length < 5) return res.status(404).send('The channel needs to have at least 5 clips')
+
+        await utils.redis.set(`rc:clips:${user[0].id}`, JSON.stringify(clips), "EX", 86400)
+    }
+
+    res.render('index', { clips })
 })
 
 async function getClips(channelID, reqs) {
