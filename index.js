d02592dba1ae86b70d77e26e1f842e0df5ab385c
Supa <36031171+0Supa@users.noreply.github.com>
2022-02-05 02:26:31 +0200

    xd

--- index.js
+++ index.js
@@ -3,55 +3,7 @@ const fs = require('fs')
 const app = express()
 const utils = require('./utils.js')
 
-app.set('view engine', 'ejs');
-
-app.use('/rcp/overlay', express.static(__dirname + '/static'))
-
-app.use('/rcp/chimiclips', express.static(__dirname + '/chimiClips'))
-
-app.get('/rcp/api/clips/:channel', async (req, res) => {
-    const channel = req.params.channel
-    if (!channel) return res.status(400).json({ error: 'you need to specify the channel name' })
-
-    const user = (await utils.helix(`users?login=${channel}`)).body.data
-    if (!user.length) return res.status(400).json({ error: 'invalid channel name' })
-
-    let clips
-    const cacheData = await utils.redis.get(`rc:clips:${user[0].id}`)
-    if (cacheData) {
-        clips = JSON.parse(cacheData)
-    } else {
-        clips = await getClips(user[0].id, 3)
-        if (clips.length < 5) return res.status(404).json({ error: 'the channel needs to have at least 5 clips' })
-
-        await utils.redis.set(`rc:clips:${user[0].id}`, JSON.stringify(clips), "EX", 86400)
-    }
-
-    res.json(clips)
-})
-
-app.get('/rcp/:channel', async (req, res) => {
-    const channel = req.params.channel
-    if (!channel) return res.status(400).send('You need to specify the channel name')
-
-    const user = (await utils.helix(`users?login=${channel}`)).body.data
-    if (!user.length) return res.status(400).send('Invalid channel name')
-
-    let clips
-    const cacheData = await utils.redis.get(`rc:clips:${user[0].id}`)
-    if (cacheData) {
-        clips = JSON.parse(cacheData)
-    } else {
-        clips = await getClips(user[0].id, 3)
-        if (clips.length < 5) return res.status(404).send('The channel needs to have at least 5 clips')
-
-        await utils.redis.set(`rc:clips:${user[0].id}`, JSON.stringify(clips), "EX", 86400)
-    }
-
-    res.render('index', { clips })
-})
-
-async function getClips(channelID, reqs) {
+const getClips = async (channelID, reqs) => {
     let i = 0
     let clips = []
     let cursor
@@ -80,6 +32,35 @@ async function getClips(channelID, reqs) {
     }
     return clips;
 }
+
+app.use('/rcp/overlay', express.static(__dirname + '/static'))
+
+app.use('/rcp/chimiclips', express.static(__dirname + '/chimiClips'))
+
+app.get('/rcp/api/clips/:channel', async (req, res) => {
+    try {
+        const channel = req.params.channel
+        if (!channel) return res.status(400).json({ error: 'you need to specify the channel name' })
+
+        const user = (await utils.helix(`users?login=${channel}`)).body.data
+        if (!user.length) return res.status(400).json({ error: 'invalid channel name' })
+
+        let clips
+        const cacheData = await utils.redis.get(`rc:clips:${user[0].id}`)
+        if (cacheData) {
+            clips = JSON.parse(cacheData)
+        } else {
+            clips = await getClips(user[0].id, 3)
+            if (clips.length < 5) return res.status(404).json({ error: 'the channel needs to have at least 5 clips' })
+
+            await utils.redis.set(`rc:clips:${user[0].id}`, JSON.stringify(clips), "EX", 86400)
+        }
+
+        res.json(clips)
+    } catch (err) {
+        res.sendStatus(500)
+    }
+})
 
 app.listen(3840, (err) => {
     if (err) throw err
